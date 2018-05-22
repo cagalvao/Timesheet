70600ccc1b22d54beb25434f226f44c41ca0478a
cagalvao <cassio.galvao@concretesolutions.com.br>
2018-05-22 03:35:01 -0300

    removing unnecessary securityDefinitions from swagger config

--- back_end/swagger/index.js
+++ back_end/swagger/index.js
@@ -3,21 +3,14 @@ function getSwaggerBase (basePath) {
   return {
     swagger: '2.0',
     info: {
-      title: 'UVA Mono',
-      description: 'Endpoints para a API da aplicação de Monografia',
+      title: 'Timesheet',
+      description: 'Endpoints para a API da aplicação de Monografia, Timesheet',
       version: '1.0.0'
     },
     basePath: '/',
     host: 'localhost:4000',
     paths: {
       $ref: basePath + '/paths.json'
-    },
-    securityDefinitions: {
-      Authorization: {
-        type: 'apiKey',
-        name: 'Authorization',
-        in: 'header'
-      }
     },
     definitions: {
       $ref: basePath + '/definitions.json'
