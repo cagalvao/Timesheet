4fa43a9b09cbb5240d48f0b3bbc5737fab795607
cagalvao <cassio.galvao@concretesolutions.com.br>
2018-06-02 00:53:27 -0300

    removing front end container for a while

--- start.sh
+++ start.sh
@@ -5,10 +5,10 @@ cd back_end
 npm run refresh_docker_volume
 npm run swagger
 
-cd ../front_end
-npm run refresh_docker_volume
+# cd ../front_end
+# npm run refresh_docker_volume
 
 docker-compose up -d mono_db
 docker-compose up -d mono_api
-docker-compose up -d mono_fe
+# docker-compose up -d mono_fe
 docker-compose up -d swagger
